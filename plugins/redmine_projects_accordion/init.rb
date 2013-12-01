4d949821b49a9f978b701a79fb917a52ab7bc7db
Dave Carlton <davec@playnet.com>
2013-11-30 22:29:09 -0600

    Added project page accordian

---
+++ plugins/redmine_projects_accordion/init.rb
@@ -0,0 +1,17 @@
+Redmine::Plugin.register :redmine_projects_accordion do
+  name 'Redmine Projects Accordion plugin'
+  author 'Reuben Mallaby'
+  description 'This is a Redmine plugin which will turn the projects page into an accordion view using jQuery UI accordion code'
+  version '0.1.0'
+  url 'http://reuben.mallaby.me'
+  author_url 'http://reuben.mallaby.me'
+end
+
+class RedmineProjectsAccordionListener < Redmine::Hook::ViewListener
+
+  # Adds javascript and stylesheet tags
+  def view_layouts_base_html_head(context)
+    stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_projects_accordion)
+  end
+  
+end
